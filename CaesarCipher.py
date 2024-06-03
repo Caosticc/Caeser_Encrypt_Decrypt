d433efac43efa348cf3d612364da09506f5bbdae
Caosticc <109768693+Caosticc@users.noreply.github.com>
2024-06-02 21:50:44 -0300

    Update and rename Caesar_Encrypt_Decrypt.py to CaesarCipher.py and logical error fixed
    logical error fixed

---
+++ CaesarCipher.py
@@ -0,0 +1,39 @@
+print('-='*25)
+print("\t\033[3;35mWelcome to Caesar Cipher Decryptor\033[m")
+print('-='*25)
+
+
+def encrypt(text, key):
+    for char in text:  # get the characters from the text
+        # transform characters into unicode table codes and sum with key
+        uni_transform = ord(char) + (key % 26)
+        # transforms new unicode table values ​​into character
+        encrypt = chr(uni_transform)
+        print(f"\033[30m{encrypt}\033[m", end='')
+    print('\nBye!!!!')
+
+
+def decrypt(text, key):
+    for char in text:  # get the characters from the text
+        # transform characters into unicode table codes and substracts with the key
+        transform_uni = ord(char) - (key % 26)
+        # transforms the unicode table values into the decrypted message
+        decrypt = chr(transform_uni)
+        print(decrypt, end='')
+    print("\nBye!!!!")
+
+
+print('''\033[1mChoose an option\033[m
+\033[34m[1] - Encrypt
+[2] - Decrypt\033[m''')
+option = int(input("Choose one: "))
+if option == 1:
+    text = input("Write the text to encrypt: ")
+    key = int(input("Put the key to encrypt: "))
+    encrypt(text, key)
+elif option == 2:
+    text = input("White the text to decrypt: ")
+    key = int(input("Put the key to decrypt: "))
+    decrypt(text, key)
+else:
+    print('Wrong option!!!')
