5342828e0f7770b45b676518355b4056578703b2
Caosticc <109768693+Caosticc@users.noreply.github.com>
2024-06-09 10:28:23 -0300

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,49 +1,51 @@
 print('-=' * 25)
-print("\t\033[3;35mWelcome to Caesar Cipher Decryptor\033[m")
+print("\t\033[3;35mBem-vindo ao Decifrador de Cifra de César\033[m")
 print('-=' * 25)
 
 
-def encrypt(text, key):
-    result = ''
-    for char in text:
+def encrypt(texto, chave):
+    resultado = ''
+    for char in texto:
         if char.isalpha():
-            shift = key % 26
+            deslocamento = chave % 26
             if char.islower():
-                result += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+                resultado += chr((ord(char) - ord('a') + deslocamento) % 26 + ord('a'))
             elif char.isupper():
-                result += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+                resultado += chr((ord(char) - ord('A') + deslocamento) % 26 + ord('A'))
         else:
-            result += char
-    print(f"\033[30m{result}\033[m")
-    print('Bye!!!!')
+            resultado += char
+    return resultado
 
 
-def decrypt(text, key):
-    result = ''
-    for char in text:
+def decrypt(texto, chave):
+    resultado = ''
+    for char in texto:
         if char.isalpha():
-            shift = key % 26
+            deslocamento = chave % 26
             if char.islower():
-                result += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+                resultado += chr((ord(char) - ord('a') - deslocamento) % 26 + ord('a'))
             elif char.isupper():
-                result += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+                resultado += chr((ord(char) - ord('A') - deslocamento) % 26 + ord('A'))
         else:
-            result += char
-    print(result)
-    print('Bye!!!!')
+            resultado += char
+    return resultado
 
 
-print('''\033[1mChoose an option\033[m
-\033[34m[1] - Encrypt
-[2] - Decrypt\033[m''')
-option = int(input("Choose one: "))
-if option == 1:
-    text = input("Write the text to encrypt: ")
-    key = int(input("Put the key to encrypt: "))
-    encrypt(text, key)
-elif option == 2:
-    text = input("Write the text to decrypt: ")
-    key = int(input("Put the key to decrypt: "))
-    decrypt(text, key)
+print('''\033[1mEscolha uma opção\033[m
+\033[34m[1] - Criptografar
+[2] - Descriptografar\033[m''')
+opcao = int(input("Escolha uma: "))
+if opcao == 1:
+    texto = input("Digite o texto para criptografar: ")
+    chave = int(input("Digite a chave para criptografar: "))
+    criptografado = encrypt(texto, chave)
+    print(f"\033[30m{criptografado}\033[m")
+    print('Tchau!!!!')
+elif opcao == 2:
+    texto = input("Digite o texto para descriptografar: ")
+    chave = int(input("Digite a chave para descriptografar: "))
+    descriptografado = decrypt(texto, chave)
+    print(descriptografado)
+    print('Tchau!!!!')
 else:
-    print('Wrong option!!!')
+    print('Opção errada!!!')
